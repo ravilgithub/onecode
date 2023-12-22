0e2ac0e788802e65287857945421bda91694dc03
Ravil <foragetonly800@mail.ru>
2023-12-22 10:36:44 +0300

    Валидировать контент компотента Trix editor
    Форма не будет отправлена, если компоненту передан параметр
    "prevent" и при этом поле редактора Trix пусто.

--- resources/views/components/forms/inputs/trix.blade.php
+++ resources/views/components/forms/inputs/trix.blade.php
@@ -6,6 +6,7 @@
     'label'       => __( 'Content' ),
     'required'    => false,
     'autofocus'   => false,
+    'prevent'     => false,
 ])
 
 <label
@@ -39,4 +40,83 @@
 
 @pushOnce('js')
     <script type="text/javascript" src="https://unpkg.com/trix@2.0.0/dist/trix.umd.min.js"></script>
+
+    @if($prevent)
+        <script type="module">
+            /**
+             * Проверка на отсутствие контента.
+             *
+             * @property {object|null} trix - объект Trix.
+             * 	Default: null
+             */
+            const trixValidate = {
+                trix: null,
+
+                /**
+                 * Получить объект Trix.
+                 *
+                 * @return {void}
+                 */
+                getTrix() {
+                    const data = {{ Illuminate\Support\Js::from([ 'id' => $id ]) }};
+
+                    if ('id' in data) {
+                        this.trix = document.querySelector(`trix-editor[input="${data.id}"]`);
+                    }
+                },
+
+                /**
+                 * Обработать событие.
+                 *
+                 * @param {object} event
+                 * @return {void|bool}
+                 */
+                submitHandler(event) {
+                    event.preventDefault();
+                    const content = this.trix.editor.getDocument().toString();
+
+                    // console.log('trix', this.trix);
+                    // console.log('content', content);
+                    // console.log('content length', content.length);
+                    // console.log('content type', typeof content);
+                    // console.log('content to bool', !!content);
+
+                    // Если только перевод на новую строку - контент пуст.
+                    if (content == '\n') {
+                        console.log('Trix editor is empty!');
+                        return false;
+                    }
+
+                    event.target.submit();
+                },
+
+                /**
+                 * Прослушать событие "submit" формы.
+                 *
+                 * @param {function} handler
+                 * @return {void}
+                 */
+                addFormEvent(handler) {
+                    if (!this.trix) {
+                        return false;
+                    }
+
+                    const ownForm = this.trix.closest('form');
+                    ownForm.addEventListener('submit', handler, false);
+                },
+
+                /**
+                 * Инициализация.
+                 *
+                 * @return {void}
+                 */
+                init() {
+                    this.getTrix();
+                    this.addFormEvent(this.submitHandler.bind(this));
+                }
+            };
+
+            trixValidate.init();
+        </script>
+    @endif
 @endPushOnce
