d0cb5ca8247df5ea80513f24b7741a0b26216647
Ravil <foragetonly800@mail.ru>
2023-12-23 14:49:48 +0300

    Добавить компонент "элемент формы text"

---
+++ resources/views/components/forms/inputs/text.blade.php
@@ -0,0 +1,28 @@
+@props([
+    'label'     => __('Title'),
+    'name'      => 'title',
+    'value'     => '',
+    'required'  => false,
+    'autofocus' => false,
+])
+
+<label
+    {{ $attributes->class([
+        'form-label',
+        'required' => $required,
+    ]) }}
+>
+    {{ $label }}
+</label>
+
+<input
+    type="text"
+    @required($required)
+    {{ $attributes->class([
+        'form-control',
+    ])->merge([
+        'name'      => $name,
+        'value'     => $value,
+        'autofocus' => $autofocus,
+    ]) }}
+/>
