dbf0aac9ed700ced5f03e27ee2cf729d36f38f6f
Ravil <foragetonly800@mail.ru>
2024-02-21 08:51:56 +0300

    Реализавать форму фильтрации записей блога

---
+++ resources/views/blog/filter.blade.php
@@ -0,0 +1,33 @@
+<x-forms.form
+    class="mb-4"
+    action="{{ route('blog.index') }}"
+    method="GET"
+>
+    <div class="row">
+        <x-forms.form-item class="col-md-4">
+            <x-forms.inputs.text
+                name="search"
+                value="{{ request('search') }}"
+                placeholder="{{ __('Поиск') }}"
+                autofocus
+                required
+            />
+        </x-forms.form-item>
+
+        <x-forms.form-item class="col-md-4">
+            <x-forms.inputs.select
+                name="category_id"
+                value="{{ request('category_id') }}"
+                :options="$categories"
+                single
+                required
+            />
+        </x-forms.form-item>
+
+        <x-forms.form-item class="col-md-4">
+            <x-button class="w-100">
+                {{ __('Применить') }}
+            </x-button>
+        </x-forms.form-item>
+    </div>
+</x-forms.form>
