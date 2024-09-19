67811c4298a5f9bedbaf37df11a56611c025f50a
Ravil <foragetonly800@mail.ru>
2024-09-19 06:02:59 +0300

    Добавить столбец admin в таблицу users

--- database/migrations/2024_09_19_022205_add_admin_field_to_users_table.php
+++ database/migrations/2024_09_19_022205_add_admin_field_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->boolean('admin')->default(false)->after('active');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('admin');
         });
     }
 };
