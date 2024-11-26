0759fcd69e2ea191b36cc95561356366a8dfb776
Ravil <foragetonly800@mail.ru>
2024-11-27 02:20:48 +0300

    Описать API для получения модели "currency"

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
+use App\Models\Currency;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,9 @@ use App\Http\Controllers\Api\PostController;
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::get('/posts/{post}/like', [PostController::class, 'like'])->name('posts.like');
+
+/**
+ * Запрос на получение тестовой модели Currency
+ * - localhost/api/currency
+ */
+Route::get('/currency', fn() => Currency::first());
