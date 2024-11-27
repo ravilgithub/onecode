a8511908575314131573da3f240779e625ca5773
Ravil <foragetonly800@mail.ru>
2024-11-28 01:38:41 +0300

    Описать модель "post"

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'content',
+        'published',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'published'    => 'boolean',
+        'published_at' => 'datetime',
+    ];
 }
