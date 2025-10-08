1bb237adafdaa280bbc313c95b447bde2019829f
Ravil <foragetonly800@mail.ru>
2025-10-08 19:33:57 +0300

    Валидировать форму регистрации пользователя

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
@@ -15,12 +16,13 @@ class RegisterController extends Controller
 
     public function store(Request $request): RedirectResponse
     {
-        if ($invalid = true) {
-            alert('Something wrong!', 'danger');
-            return back()->withInput();
-        }
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'max:50'],
+            'email' => ['required', 'string', 'email', 'unique:users', 'max:50'],
+            'password' => ['required', 'string', 'confirmed', 'min:7', 'max:50'],
+            'rules' => ['accepted'],
+        ]);
 
-        alert('Welcome aboard.');
         return redirect()->route('user.posts.index');
     }
 }
