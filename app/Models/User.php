6d9115e7064d48dac3c6dfc0b1802460273d8392
Ravil <foragetonly800@mail.ru>
2025-10-09 23:45:01 +0300

    Создать свойства по умолчанию для модели "users"

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,11 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $attributes = [
+        'active' => true,
+        'admin'  => false,
+    ];
+
     /**
      * The attributes that are mass assignable.
      *
