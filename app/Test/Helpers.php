5790a5253173dc960b3ea07dcdf235ae6b77ab6a
Ravil <foragetonly800@mail.ru>
2025-10-10 16:35:21 +0300

    Создать поле формы типа date

--- app/Test/Helpers.php
+++ app/Test/Helpers.php
@@ -53,3 +53,27 @@ if (! function_exists('getPosts')) {
         return array_fill(0, 4, $post);
     }
 }
+
+/**
+ * Если значение поля массив(name="delivery[date]"),
+ * то функция вернёт массив состоящий из имени поля и имени параметра.
+ *
+ * Функция вернёт имя поля если оно не является массивом.
+ */
+if (! function_exists('separate_input_name')) {
+    function separate_input_name(string $data) : string|array
+    {
+        $input = [];
+
+        // Получить имя массива если это массив
+        if ($open_pos = strpos($data, '[')) {
+            $input['name'] = mb_substr($data, 0, $open_pos);
+
+            // Получаем имя параметра если он есть
+            if ( ($open_pos + 1) !== ($close_pos = strpos($data, ']')))
+                $input['param'] = mb_substr($data, $open_pos + 1, $close_pos - 1 - $open_pos);
+        }
+
+        return $input ?: $data;
+    }
+}
